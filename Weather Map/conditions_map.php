d5ec4daa23bf72611ad68d3a2f8eeb94db8aacd7
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-17 22:48:05 +0200

    Added the page conditions_map.php

---
+++ Weather Map/conditions_map.php
@@ -0,0 +1,25 @@
+<?php
+
+// Includes
+require_once('src/BusinessLogic/ConditionsMapGenerator.php');
+
+// Get the HTTP parameters
+$dateHttpParameter = $_GET['date'];
+
+if (isset($dateHttpParameter) && WeatherMap\BusinessLogic\HttpParameterValidator::hasValue($dateHttpParameter)) {
+    $date = strtotime($dateHttpParameter);
+}
+
+// Generate the map
+$mapGenerator = new WeatherMap\BusinessLogic\ConditionsMapGenerator($date);
+$map = $mapGenerator->generateMap();
+
+// Set the HTTP headers
+header("Content-Type: image/jpg");
+header("Content-Length: " . 10000);
+
+// Return the map
+imagejpeg($map);
+imagedestroy($map);
+
+?>
