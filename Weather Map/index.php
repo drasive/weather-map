61385c8e0a58d2b313848bacfe6129826019be77
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-17 17:18:59 +0200

    Improved the design of the image slider

--- Weather Map/index.php
+++ Weather Map/index.php
@@ -6,7 +6,7 @@
     <meta name="author" content="Dimitri Vranken" />
 
     <title>Wetterkarte</title>
-    <link rel="shortcut icon" href="media/icons/form.ico" />
+    <link rel="shortcut icon" href="media/icons/sun.ico" />
 
     <!-- jQuery -->
     <script src="http://code.jquery.com/jquery.js"></script>
@@ -22,7 +22,7 @@
 
     <!-- Flex Slider -->
     <link rel="stylesheet" href="style/css/flexslider.css" type="text/css" />
-    <script src="scripts/js/jquery.flexslider.js"></script>
+    <script src="scripts/js/jquery.flexslider.js" type="text/javascript"></script>
 
     <script type="text/javascript" charset="utf-8">
         $(window).load(function () {
@@ -30,8 +30,7 @@
                 slideshow: false,
                 animation: "slide",
                 easing: "easeInElastic",
-                animationLoop: true,
-                controlNav: "thumbnails"
+                animationLoop: true
             });
         });
 
@@ -40,35 +39,47 @@
 </head>
 <body>
     <?php
-
-    // TODO: Debug flag
-    $debug = false;
-
     require_once('includes/warnings.inc.php');
     require_once('includes/navigation.inc.html');
-
-    require_once('src/BusinessLogic/ConfigurationReader.php');
-
     ?>
-
+    
     <?php
-
-
-
+    // TODO: Debug flag
+    $debug = false;
+        
+    require_once('src/BusinessLogic/ConfigurationReader.php');
     ?>
 
-    <div class="flexslider">
-        <ul class="slides">
-            <li data-thumb="media/images/batman_ironman_spider-man.jpg">
-                <img src="media/images/batman_ironman_spider-man.jpg" />
-                Test
-            </li>
-            <li data-thumb="media/images/kid_lolly_cat.jpg">
-                <img src="media/images/kid_lolly_cat.jpg" />
-                <p class="flex-caption">Test 2</p>
-            </li>
-        </ul>
+    <div class="content">
+        <div class="title-box text-center">
+            <h1>Today</h1>
+        </div>
+        <div class="container">
+            <article>
+                <section>
+                    <div class="flexslider">
+                        <ul class="slides">
+                            <li>
+                                <h2>Conditions</h2>
+                                <img src="media/images/batman_ironman_spider-man.jpg" />
+                            </li>
+                            <li>
+                                <h2>Temperature</h2>
+                                <img src="media/images/kid_lolly_cat.jpg" />
+                            </li>
+                            <li>
+                                <h2>Wind</h2>
+                                <img src="media/images/kid_lolly_cat.jpg" />
+                            </li>
+                            <li>
+                                <h2>Pollen</h2>
+                                <img src="media/images/kid_lolly_cat.jpg" />
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </article>
+        </div>
     </div>
-
 </body>
 </html>
