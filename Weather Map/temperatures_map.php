7ab53c4c167cc63f1cee2b1d522c2709baeb354a
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-18 13:21:04 +0200

    Added the pages temperatures_map.php, wind_map.php and pollination_map.php

---
+++ Weather Map/temperatures_map.php
@@ -0,0 +1,24 @@
+<?php
+
+// Includes
+require_once('src/BusinessLogic/TemperaturesMapGenerator.php');
+
+// Get the HTTP parameters
+$dateHttpParameter = $_GET['date'];
+
+if (isset($dateHttpParameter) && WeatherMap\BusinessLogic\HttpParameterValidator::hasValue($dateHttpParameter)) {
+    $date = strtotime($dateHttpParameter);
+}
+
+// Generate the map
+$mapGenerator = new WeatherMap\BusinessLogic\TemperaturesMapGenerator($date);
+$map = $mapGenerator->generateMap();
+
+// Set the HTTP headers
+header("Content-Type: image/jpg");
+
+// Return the map
+imagejpeg($map);
+imagedestroy($map);
+
+?>
