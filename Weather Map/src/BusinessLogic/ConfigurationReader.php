873d57bd6fb37a7eb8c6d6467c9606a63ce36d23
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-18 13:04:28 +0200

    Added the key "debugMode" to the configuration.ini

--- Weather Map/src/BusinessLogic/ConfigurationReader.php
+++ Weather Map/src/BusinessLogic/ConfigurationReader.php
@@ -7,20 +7,25 @@
           // Variables
           protected static $m_configurationFile = 'configuration.ini';
           
-          protected static $m_section_general = 'general';
+          protected static $m_sectionGeneral = 'general';
+          protected static $m_debugMode = 'debugMode';
           protected static $m_timezone = 'timezone';
           
-          protected static $m_section_webservice = 'webservice';
-          protected static $m_webservice_URL = 'URL';
+          protected static $m_sectionWebservice = 'webservice';
+          protected static $m_webserviceURL = 'URL';
           
           // General section
+          public static function getDebugMode(){ 
+              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionGeneral][self::$m_debugMode];
+          }
+          
           public static function getTimezone(){ 
-              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_section_general][self::$m_timezone];
+              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionGeneral][self::$m_timezone];
           }
           
           // Webservice section
           public static function getWebserviceURL(){ 
-              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_section_webservice][self::$m_webservice_URL];
+              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionWebservice][self::$m_webserviceURL];
           }
           
       }
