b96bf294b0cafc53ec9eba9dc463bf2933b01230
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-18 23:13:09 +0200

    Minor bugfix

--- Weather Map/wind_map.php
+++ Weather Map/wind_map.php
@@ -11,8 +11,8 @@ if (isset($dateHttpParameter) && \WeatherMap\BusinessLogic\HttpParameterValidato
 }
 
 // Generate the map
-$mapGenerator = new \WeatherMap\BusinessLogic\WindMapGenerator($date);
-$map = $mapGenerator->generateMap();
+$mapGenerator = new \WeatherMap\BusinessLogic\WindMapGenerator();
+$map = $mapGenerator->generateMap($date);
 
 // Set the HTTP headers
 header("Content-Type: image/jpg");
