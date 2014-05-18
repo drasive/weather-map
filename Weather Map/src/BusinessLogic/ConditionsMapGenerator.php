7bc591a7d4b6d336f7bfc850285142384ed1746b
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-18 13:17:46 +0200

    Updated the ConditionsMapGenerator to use the updated DataReaders and WeatherDataBuilders

--- Weather Map/src/BusinessLogic/ConditionsMapGenerator.php
+++ Weather Map/src/BusinessLogic/ConditionsMapGenerator.php
@@ -1,34 +1,46 @@
 <?php namespace WeatherMap\BusinessLogic;
 
-       require_once('src/BusinessLogic/WeatherMapGenerator.php');
-       require_once('src/BusinessLogic/ConfigurationReader.php');       
-       require_once('src/BusinessLogic/WeatherDataFromCsvBuilder.php');
-       require_once('src/DataAccess/CsvFileReader.php');
-       require_once('src/UserInterface/ConditionsMapGenerator.php');
-      
+      require_once('src/BusinessLogic/WeatherMapGenerator.php');
+
+      require_once('src/DataAccess/WebsiteWeatherDataReader.php');
+      require_once('src/BusinessLogic/ConfigurationReader.php');
+      require_once('src/BusinessLogic/ArrayWeatherDataBuilder.php');
+      require_once('src/UserInterface/ConditionsMapGenerator.php');
+
       class ConditionsMapGenerator extends WeatherMapGenerator {
-          
+
           // Public constructors
           function __construct($date) {
               parent::__construct($date);
           }
-          
+
           // Public methods
           public function generateMap() {
-              $webserviceURL = \WeatherMap\BusinessLogic\ConfigurationReader::getWebserviceURL();
-              // TODO: Fix read
-              $weatherDataFile = file_get_contents($webserviceURL);
-              $weatherDataArray = \WeatherMap\DataAccess\CsvFileReader::readFile($weatherDataFile);
-              $weatherData = \WeatherMap\BusinessLogic\WeatherDataFromCsvBuilder::buildWeatherData($weatherDataArray);
-              
+              // Get raw data
+              $weatherDataReader = new \WeatherMap\DataAccess\WebsiteWeatherDataReader();
+              $weatherDataSource = \WeatherMap\BusinessLogic\ConfigurationReader::getWebserviceURL();
+              $weatherDataUnparsed = $weatherDataReader->readWeatherData($weatherDataSource);
+
+              if (\WeatherMap\BusinessLogic\ConfigurationReader::getDebugMode()) {
+                  // TODO: Print debug information
+              }
+
+              // Parse data
+              $weatherDataBuilder = new \WeatherMap\BusinessLogic\ArrayWeatherDataBuilder();
+              $weatherData = $weatherDataBuilder->buildWeatherData($weatherDataUnparsed);
+
+              if (\WeatherMap\BusinessLogic\ConfigurationReader::getDebugMode()) {
+                  // TODO: Print debug information
+              }
+
               // Generate map
               $mapGenerator = new \WeatherMap\UserInterface\ConditionsMapGenerator($this->date);
               $map = $mapGenerator->generateMap($weatherData);
-              
+
               // Return map
               return $map;
           }
-          
+
       }
 
 ?>
--- Weather Map/src/BusinessLogic/WeatherMapGenerator.php
+++ Weather Map/src/BusinessLogic/WeatherMapGenerator.php
@@ -8,7 +8,7 @@
           // Public properties
           public function __get($property) {
               if (property_exists($this, $property)) {
-                  return $this->$property;
+                  return $this->property;
               }
           }
           
