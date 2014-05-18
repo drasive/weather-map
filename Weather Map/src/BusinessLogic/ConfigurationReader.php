02113e1abf20bf754a089895a9af0686f742542a
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-18 16:24:50 +0200

    Added a release date to the configuraion.ini

--- Weather Map/src/BusinessLogic/ConfigurationReader.php
+++ Weather Map/src/BusinessLogic/ConfigurationReader.php
@@ -7,18 +7,26 @@
           // Variables
           protected static $m_configurationFile = 'configuration.ini';
           
-          protected static $m_sectionGeneral = 'general';
+          protected static $m_sectioRelease = 'release';
           protected static $m_debugMode = 'debugMode';
+          protected static $m_releaseDate = 'releaseDate';
+          
+          protected static $m_sectionGeneral = 'general';
           protected static $m_timezone = 'timezone';
           
           protected static $m_sectionWebservice = 'webservice';
           protected static $m_webserviceURL = 'URL';
           
-          // General section
+          // Release section
           public static function getDebugMode(){ 
-              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionGeneral][self::$m_debugMode];
+              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectioRelease][self::$m_debugMode];
           }
           
+          public static function getReleaseDate(){ 
+              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectioRelease][self::$m_releaseDate];
+          }
+          
+          // General section
           public static function getTimezone(){ 
               return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionGeneral][self::$m_timezone];
           }
