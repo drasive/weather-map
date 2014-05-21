b2e7be9977ab389651ad9ac0f530d4d8cee61e86
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-21 22:39:19 +0200

    Added http-parameter validation in the php scripts called to output the weather maps

--- Weather Map/conditions_map.php
+++ Weather Map/conditions_map.php
@@ -1,24 +1,35 @@
 <?php
 
+// Set the HTTP headers
+header("Content-Type: image/png");
+
 // Includes
 require_once('src/BusinessLogic/ConditionsMapGenerator.php');
+require_once('src/BusinessLogic/ParameterValidator.php');
 
 // Get the HTTP parameters
-// TODO: Ceck if date is smaller than today or bigger than allowed -> use today (currect URL if possible)
-
 $dateHttpParameter = $_GET['date'];
+$date = null;
 
 if (isset($dateHttpParameter) && \WeatherMap\HttpParameterHelper::hasValue($dateHttpParameter)) {
     $date = strtotime($dateHttpParameter);
 }
-// TODO: Else -> throw exception or something
+
+// Validate the HTTP parameters
+$invalidDate = time() - (60 * 60 * 24);
+
+if ($date == null || $date == false || date == '' ||
+    $date != \WeatherMap\BusinessLogic\ParameterValidator::validateRequestedMapDate($date, $invalidDate)) { // Date couldn't be obtained or is invalid
+    $argumentInvalid = imagecreatefrompng('media/images/argument_invalid.png');
+    imagepng($argumentInvalid);
+    imagedestroy($argumentInvalid);
+    
+    exit();
+}
 
 // Generate the map
 $mapGenerator = new \WeatherMap\BusinessLogic\ConditionsMapGenerator();
 $map = $mapGenerator->generateMap($date);
-
-// Set the HTTP headers
-header("Content-Type: image/jpg");
 
 // Return the map
 imagepng($map);
--- Weather Map/pollination_map.php
+++ Weather Map/pollination_map.php
@@ -1,21 +1,35 @@
 <?php
 
+// Set the HTTP headers
+header("Content-Type: image/png");
+
 // Includes
 require_once('src/BusinessLogic/PollinationMapGenerator.php');
+require_once('src/BusinessLogic/ParameterValidator.php');
 
 // Get the HTTP parameters
 $dateHttpParameter = $_GET['date'];
+$date = null;
 
 if (isset($dateHttpParameter) && \WeatherMap\HttpParameterHelper::hasValue($dateHttpParameter)) {
     $date = strtotime($dateHttpParameter);
 }
 
+// Validate the HTTP parameters
+$invalidDate = time() - (60 * 60 * 24);
+
+if ($date == null || $date == false || date == '' ||
+    $date != \WeatherMap\BusinessLogic\ParameterValidator::validateRequestedMapDate($date, $invalidDate)) { // Date couldn't be obtained or is invalid
+    $argumentInvalid = imagecreatefrompng('media/images/argument_invalid.png');
+    imagepng($argumentInvalid);
+    imagedestroy($argumentInvalid);
+    
+    exit();
+}
+
 // Generate the map
 $mapGenerator = new \WeatherMap\BusinessLogic\PollinationMapGenerator();
 $map = $mapGenerator->generateMap($date);
-
-// Set the HTTP headers
-header("Content-Type: image/jpg");
 
 // Return the map
 imagepng($map);
