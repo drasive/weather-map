dc9b495bcb127fa419f0541187fe7dc51c32490f
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-21 17:43:56 +0200

    Removed the section "general" from the configuration

--- Weather Map/src/BusinessLogic/ConfigurationReader.php
+++ Weather Map/src/BusinessLogic/ConfigurationReader.php
@@ -10,10 +10,7 @@
           protected static $m_sectioRelease = 'release';
           protected static $m_debugMode = 'debugMode';
           protected static $m_releaseDate = 'releaseDate';
-          
-          protected static $m_sectionGeneral = 'general';
-          protected static $m_timezone = 'timezone';
-          
+                    
           protected static $m_sectionWebservice = 'webservice';
           protected static $m_webserviceURL = 'URL';
           
@@ -25,12 +22,7 @@
           public static function getReleaseDate(){ 
               return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectioRelease][self::$m_releaseDate];
           }
-          
-          // General section
-          public static function getTimezone(){ 
-              return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionGeneral][self::$m_timezone];
-          }
-          
+                    
           // Webservice section
           public static function getWebserviceURL(){ 
               return \WeatherMap\DataAccess\ConfigurationReader::readIniFile(self::$m_configurationFile, true)[self::$m_sectionWebservice][self::$m_webserviceURL];
