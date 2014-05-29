a14059307a04c60ed69cfc91e68f1d08372c2ab7
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-05-29 21:45:58 +0200

    Removed the unused CacheManager

--- Weather Map/index.php
+++ Weather Map/index.php
@@ -6,7 +6,6 @@
 // TODO: Check if the webservice can be reached or show an error
 
 // Includes
-require_once('src/BusinessLogic/CacheManager.php');
 require_once('src/BusinessLogic/DateTimeHelper.php');
 require_once('src/BusinessLogic/ParameterValidator.php');
 require_once('src/BusinessLogic/ConfigurationReader.php');
