2ef64f07d04682845b3645addffd09922a2ad64b
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-08-14 16:37:23 +0200

    Removed the version control management tools

--- Git/Azure - push master.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set PATH=%PATH%;"%SYSTEMDRIVE%\Program Files (x86)\Git\bin\"
-
-git push -f azure master:master
-
-echo.
-echo.
-pause
--- Git/_start bash.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cd ..
-start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
